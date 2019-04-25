2a1c4b72228fae9ea02df545eba448ac10c9ca7c
JinYue <jy1212@qq.com>
2019-04-25 16:25:35 +0800

    add a anim

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def hello(name=None):
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'waterm.png', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.run(
