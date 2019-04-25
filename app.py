167b6b31ec994f40ff4cb4ee152ffd5f80cee7a8
JinYue <jy1212@qq.com>
2019-04-25 16:29:38 +0800

    change web icon

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def hello(name=None):
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'waterm.png', mimetype='image/vnd.microsoft.icon')
+                               'favicon.png', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.run(
